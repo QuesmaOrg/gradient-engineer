4ecff05e9d9b01e46e6470345e7e943644d8bee3
Piotr Grabowski <piotr@quesma.com>
2025-08-29 23:15:00 +0200

    Revert "snapshot"
    This reverts commit 7e4d3b4e143d2310a3732336046b273be2be66c4.

--- app/main.go
+++ app/main.go
@@ -4,17 +4,15 @@ import (
 	"log"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"gradient-engineer/toolbox"
 )
 
 func main() {
 	// Create a new toolbox instance
-	tb := toolbox.NewToolbox("https://gradient.engineer/toolbox_v0.tar.xz")
-	
-	defer tb.Cleanup()
+	toolbox := NewToolbox("https://gradient.engineer/toolbox_v0.tar.xz")
+	defer toolbox.Cleanup()
 
 	// Create and run the Bubble Tea program which will handle toolbox download and diagnostics
-	p := tea.NewProgram(NewModel(tb), tea.WithMouseCellMotion())
+	p := tea.NewProgram(NewModel(toolbox), tea.WithMouseCellMotion())
 	if _, err := p.Run(); err != nil {
 		log.Fatalf("Error running Bubble Tea program: %v", err)
 	}
