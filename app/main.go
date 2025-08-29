7e4d3b4e143d2310a3732336046b273be2be66c4
Piotr Grabowski <piotr@quesma.com>
2025-08-29 23:11:06 +0200

    snapshot

--- app/main.go
+++ app/main.go
@@ -4,15 +4,17 @@ import (
 	"log"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"gradient-engineer/toolbox"
 )
 
 func main() {
 	// Create a new toolbox instance
-	toolbox := NewToolbox("https://gradient.engineer/toolbox_v0.tar.xz")
-	defer toolbox.Cleanup()
+	tb := toolbox.NewToolbox("https://gradient.engineer/toolbox_v0.tar.xz")
+	
+	defer tb.Cleanup()
 
 	// Create and run the Bubble Tea program which will handle toolbox download and diagnostics
-	p := tea.NewProgram(NewModel(toolbox), tea.WithMouseCellMotion())
+	p := tea.NewProgram(NewModel(tb), tea.WithMouseCellMotion())
 	if _, err := p.Run(); err != nil {
 		log.Fatalf("Error running Bubble Tea program: %v", err)
 	}
